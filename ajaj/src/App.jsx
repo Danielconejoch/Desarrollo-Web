ccfdfd3f78e2bf8ba75abcaeb7311e2d7ee55e48
daniel.conejochevez <daniel.conejochevez@ucr.ac.cr>
2024-10-18 18:49:55 -0600

    cambiando cositas para que php funcione en titanic pq se veia horrible

--- ajaj/src/App.jsx
+++ ajaj/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 function App() {
   const [provincias, setProvincias] = useState([]);
@@ -42,48 +43,60 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h2>Selecciona tu ubicación:</h2>
+    <div className="App centered-container">
+      <div className="form-container">
+        <h3 className="text-left">Seleccione una ubicación</h3>
 
-      <div>
-        <label>Provincia:</label>
-        <select value={selectedProvincia} onChange={handleProvinciaChange}>
-          <option value="">Seleccione una provincia</option>
-          {provincias.map((provincia) => (
-            <option key={provincia.id} value={provincia.id}>
-              {provincia.nombre}
-            </option>
-          ))}
-        </select>
+        <form className="text-left">
+          <div className="form-group">
+            <label>Provincia:</label>
+            <select
+              className="form-control"
+              value={selectedProvincia}
+              onChange={handleProvinciaChange}
+            >
+              <option value="">Seleccione una provincia</option>
+              {provincias.map((provincia) => (
+                <option key={provincia.id} value={provincia.id}>
+                  {provincia.nombre}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          {cantones.length > 0 && (
+            <div className="form-group mt-3">
+              <label>Cantón:</label>
+              <select
+                className="form-control"
+                value={selectedCanton}
+                onChange={handleCantonChange}
+              >
+                <option value="">Seleccione un cantón</option>
+                {cantones.map((canton) => (
+                  <option key={canton.id} value={canton.id}>
+                    {canton.nombre}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+
+          {distritos.length > 0 && (
+            <div className="form-group mt-3">
+              <label>Distrito:</label>
+              <select className="form-control">
+                <option value="">Seleccione un distrito</option>
+                {distritos.map((distrito) => (
+                  <option key={distrito.id} value={distrito.id}>
+                    {distrito.nombre}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+        </form>
       </div>
-
-      {cantones.length > 0 && (
-        <div>
-          <label>Cantón:</label>
-          <select value={selectedCanton} onChange={handleCantonChange}>
-            <option value="">Seleccione un cantón</option>
-            {cantones.map((canton) => (
-              <option key={canton.id} value={canton.id}>
-                {canton.nombre}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
-
-      {distritos.length > 0 && (
-        <div>
-          <label>Distrito:</label>
-          <select>
-            <option value="">Seleccione un distrito</option>
-            {distritos.map((distrito) => (
-              <option key={distrito.id} value={distrito.id}>
-                {distrito.nombre}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
     </div>
   );
 }
