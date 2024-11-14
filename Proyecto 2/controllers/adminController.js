482a4a06d882102cf83ceac06d3f8df076c80325
daniel.conejochevez <daniel.conejochevez@ucr.ac.cr>
2024-11-13 20:17:10 -0600

    admin users crud completed

--- Proyecto 2/controllers/adminController.js
+++ Proyecto 2/controllers/adminController.js
@@ -1,3 +1,71 @@
+const { User } = require('../models');
+
 exports.dashboard = (req, res) => {
   res.render('admin/dashboard', { title: 'Admin Dashboard', username: req.session.username });
+};
+
+// Users
+exports.listUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.render('admin/users', { title: 'Manage Users', users });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send('Error fetching users');
+  }
+};
+
+exports.createUserForm = (req, res) => {
+  res.render('admin/createUser', { title: 'Create New User' });
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
+    console.log('Creating user with data:', { username, email, password, role });
+    await User.create({ username, email, password, role });
+    res.redirect('/admin/users');
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).send('Error creating user');
+  }
+};
+
+exports.editUserForm = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    res.render('admin/editUser', { title: 'Edit User', user });
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).send('Error fetching user');
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const username = req.params.username;
+    const { email, role } = req.body;
+
+    await User.update(
+      { email, role },
+      { where: { username } }
+    );
+
+    res.redirect('/admin/users');
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).send('Error updating user');
+  }
+};
+
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const username = req.params.username;
+    await User.destroy({ where: { username } });
+    res.redirect('/admin/users');
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).send('Error deleting user');
+  }
 };
--- Proyecto 2/routes/adminRoutes.js
+++ Proyecto 2/routes/adminRoutes.js
@@ -6,5 +6,11 @@ const { ensureAuthenticated, checkAdmin } = require('../middlewares/authMiddlewa
 const adminController = require('../controllers/adminController');
 
 router.get('/dashboard', ensureAuthenticated, checkAdmin, adminController.dashboard);
+router.get('/users', ensureAuthenticated, checkAdmin, adminController.listUsers);
+router.get('/users/create', ensureAuthenticated, checkAdmin, adminController.createUserForm);
+router.post('/users', ensureAuthenticated, checkAdmin, adminController.createUser);
+router.get('/users/:id/edit', ensureAuthenticated, checkAdmin, adminController.editUserForm);
+router.post('/users/edit/:username', ensureAuthenticated, checkAdmin, adminController.updateUser);
+router.post('/users/delete/:username', ensureAuthenticated, checkAdmin, adminController.deleteUser);
 
 module.exports = router;
